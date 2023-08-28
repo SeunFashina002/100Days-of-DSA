f2af3600647c03b8893061b93b5a998acd30b799
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-08-28 05:16:28 -0700

    python and javascript solution to pattern1 and pattern2 problem statement

---
+++ 2-Patterns/pattern1/solution.js
@@ -0,0 +1,17 @@
+/*
+
+* * * *
+* * * *
+* * * *
+* * * *
+
+*/
+
+
+
+for (let i = 0; i < 4; i++) {
+  for (let j = 0; j < 4; j++) {
+    process.stdout.write("* ");
+  }
+  console.log(""); // This console.log statement moves to the next line after each row
+}
---
+++ 2-Patterns/pattern2/solution.py
@@ -0,0 +1,16 @@
+"""
+
+*
+* *
+* * *
+* * * * 
+* * * * *
+
+"""
+
+
+
+for i in range(5):
+    for j in range(i):
+        print("*", end="")
+    print("")
