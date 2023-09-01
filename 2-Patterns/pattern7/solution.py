eafcd1657121504ce9b0c52f8e655b5b376e12a9
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-09-01 07:43:13 -0700

    updated pattern 7 - python solution

--- 2-Patterns/pattern7/solution.py
+++ 2-Patterns/pattern7/solution.py
@@ -8,13 +8,17 @@
 
 """
 n = 5
+
 for i in range(n):
+    # left spaces
     for j in range(n, i, -1):
         print("", end=" ")
     
+    # stars
     for j in range(2*i+1):
         print("*", end="")
-    
+
+    # right spaces
     for j in range(n, i, -1):
         print("", end=" ")
     
