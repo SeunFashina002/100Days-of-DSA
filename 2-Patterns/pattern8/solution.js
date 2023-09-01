ca540d72cb64f7770bc02fc045a96048d8862226
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-09-01 09:09:28 -0700

    updated pattern 8 solution

--- 2-Patterns/pattern8/solution.js
+++ 2-Patterns/pattern8/solution.js
@@ -8,22 +8,25 @@
 
 */
 
-let n = 5;
-for (let i = 0; i < 5; i++) {
-  // left spaces
-  for (let j = 0; j < i; j++) {
-    process.stdout.write(" ");
-  }
+function pattern8(n) {
+  for (let i = 0; i < 5; i++) {
+    // left spaces
+    for (let j = 0; j < i; j++) {
+      process.stdout.write(" ");
+    }
 
-  // stars
-  for (let j = 0; j < 2 * (n - i) - 1; j++) {
-    process.stdout.write("*");
-  }
+    // stars
+    for (let j = 0; j < 2 * (n - i) - 1; j++) {
+      process.stdout.write("*");
+    }
 
-  // right spaces
-  for (let j = 0; j < i; j++) {
-    process.stdout.write(" ");
-  }
+    // right spaces
+    for (let j = 0; j < i; j++) {
+      process.stdout.write(" ");
+    }
 
-  console.log("");
+    console.log("");
+  }
 }
+
+pattern8(5);
--- 2-Patterns/pattern8/solution.py
+++ 2-Patterns/pattern8/solution.py
@@ -8,19 +8,22 @@
 
 """
 
-n = 5 
 
-for i in range(n):
-    # left spaces
-    for j in range(i):
-        print("", end=" ")
+def pattern8(n):
+    for i in range(n):
+        # left spaces
+        for j in range(i):
+            print("", end=" ")
 
-    # stars
-    for j in range(2 * (n - i) - 1):
-        print("*", end="")
+        # stars
+        for j in range(2 * (n - i) - 1):
+            print("*", end="")
 
-    # right spaces
-    for j in range(i):
-        print("", end=" ")
+        # right spaces
+        for j in range(i):
+            print("", end=" ")
 
-    print("")
+        print("")
+
+
+pattern8(5)
