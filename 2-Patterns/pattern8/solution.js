8d033b59c047ef32174455c7d2159b7203f75456
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-09-01 07:44:51 -0700

    solved pattern 8 - js solution

---
+++ 2-Patterns/pattern8/solution.js
@@ -0,0 +1,29 @@
+/*  
+
+    * * * * * * * * *
+      * * * * * * *
+        * * * * * 
+          * * *
+            *     
+
+*/
+
+let n = 5;
+for (let i = 0; i < 5; i++) {
+  // left spaces
+  for (let j = 0; j < i; j++) {
+    process.stdout.write(" ");
+  }
+
+  // stars
+  for (let j = 0; j < 2 * (n - i) - 1; j++) {
+    process.stdout.write("*");
+  }
+
+  // right spaces
+  for (let j = 0; j < i; j++) {
+    process.stdout.write(" ");
+  }
+
+  console.log("");
+}
