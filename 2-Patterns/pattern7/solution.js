b4a6ce986ba705e50243a2be9714c0864b3a652b
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-09-01 06:55:44 -0700

    updated pattern 7 for js solution

--- 2-Patterns/pattern7/solution.js
+++ 2-Patterns/pattern7/solution.js
@@ -8,9 +8,10 @@
 
 */
 
-for (let i = 0; i < 5; i++) {
+let n = 5;
+for (let i = 0; i < n; i++) {
   // left space
-  for (let j = 0; j <= 5 - i; j++) {
+  for (let j = 0; j <= n - i; j++) {
     process.stdout.write(" ");
   }
 
@@ -20,7 +21,7 @@ for (let i = 0; i < 5; i++) {
   }
 
   // right space
-  for (let j = 0; j <= 5 - i; j++) {
+  for (let j = 0; j <= n - i; j++) {
     process.stdout.write(" ");
   }
 
