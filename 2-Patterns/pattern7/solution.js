b0e5053c62832b5abad4b95cb55924553be65416
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-09-01 09:08:33 -0700

    updated pattern 7 solution

--- 2-Patterns/pattern7/solution.js
+++ 2-Patterns/pattern7/solution.js
@@ -8,22 +8,25 @@
 
 */
 
-let n = 5;
-for (let i = 0; i < n; i++) {
-  // left space
-  for (let j = 0; j <= n - i; j++) {
-    process.stdout.write(" ");
-  }
+function pattern7(n) {
+  for (let i = 0; i < n; i++) {
+    // left space
+    for (let j = 0; j < n - i - 1; j++) {
+      process.stdout.write(" ");
+    }
 
-  // stars
-  for (let j = 0; j < 2 * i + 1; j++) {
-    process.stdout.write("*");
-  }
+    // stars
+    for (let j = 0; j < 2 * i + 1; j++) {
+      process.stdout.write("*");
+    }
 
-  // right space
-  for (let j = 0; j <= n - i; j++) {
-    process.stdout.write(" ");
-  }
+    // right space
+    for (let j = 0; j < n - i - 1; j++) {
+      process.stdout.write(" ");
+    }
 
-  console.log("");
+    console.log("");
+  }
 }
+
+pattern7(5);
--- 2-Patterns/pattern7/solution.py
+++ 2-Patterns/pattern7/solution.py
@@ -7,20 +7,20 @@
     * * * * * * * * *
 
 """
-n = 5
+def pattern7(n):
+  for i in range(n):
+      # left spaces
+      for j in range(n - i - 1):
+          print("", end=" ")
+      
+      # stars
+      for j in range(2*i+1):
+          print("*", end="")
 
-for i in range(n):
-    # left spaces
-    for j in range(n, i, -1):
-        print("", end=" ")
+      # right spaces
+      for j in range(n - i - 1):
+          print("", end=" ")
+      
+      print("")
     
-    # stars
-    for j in range(2*i+1):
-        print("*", end="")
-
-    # right spaces
-    for j in range(n, i, -1):
-        print("", end=" ")
-    
-    print("")
-    
+pattern7(5)
