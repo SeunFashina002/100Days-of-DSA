e9983397c1146629d3b58fcba6f336f35afebb04
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-09-04 05:17:58 -0700

    updated javascript solution

--- 2-Patterns/pattern10/solution.js
+++ 2-Patterns/pattern10/solution.js
@@ -13,10 +13,12 @@
 
 */
 
-n = 5;
-for (let i = 1; i <= 2 * n - 1; i++) {
+let n = 9; //number of rows
+let breakpoint = 5; //row where the number of stars started decreasing
+
+for (let i = 1; i <= 9; i++) {
   let stars = i;
-  if (stars > n) stars = 2 * n - i;
+  if (stars > breakpoint) stars = 2 * breakpoint - i;
   for (j = 0; j < stars; j++) {
     process.stdout.write("* ");
   }
